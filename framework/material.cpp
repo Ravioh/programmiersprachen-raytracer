6d2f83a288700e22db6ba0f12cdf8dcec82dffcb
Ravioh <killuah@gmx.net>
2015-05-31 18:58:02 +0200

    material.h and material.cpp
    Added material.h and material.cpp
    Added standard constructor in color, which is needed for material

---
+++ framework/material.cpp
@@ -0,0 +1,31 @@
+#include "material.h"
+
+Material::Material() {
+	name_ = "Standard";
+	ka_ = Color(0.0, 0.0, 0.0);
+	kd_ = Color(0.0, 0.0, 0.0);
+	ks_ = Color(0.0, 0.0, 0.0);
+	m_ = 0.0;
+}
+Material::Material(std::string name, Color ka, Color kd, Color ks, float m) : name_( name ), ka_( ka ), kd_( kd ), ks_( ks ), m_( m ) {}
+Material::~Material() { }
+
+float Material::getM() {
+	return m_;
+}
+Color Material::getKA() {
+	return ka_;
+}
+Color Material::getKD() {
+	return kd_;
+}
+Color Material::getKS() {
+	return ks_;
+}
+std::string Material::getName() {
+	return name_;
+}
+
+std::ostream& operator<<(std::ostream& os, Material& m) {
+	return os << "----------------\nMaterial:\nname: " << m.getName() << "\nka: " << m.getKA() << "ks: " << m.getKS() << "kd: " << m.getKD() << "m :" << m.getM() << std::endl;
+}
