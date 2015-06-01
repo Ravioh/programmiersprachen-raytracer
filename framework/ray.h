bf58d833facd14b3b5d9a294be300e720264dde5
Ravioh <killuah@gmx.net>
2015-06-01 11:08:59 +0200

    Corrected Ray constructor

--- framework/ray.h
+++ framework/ray.h
@@ -1,11 +1,10 @@
 #include <glm/glm.hpp>
 
 struct Ray {
-	Ray() { 
-		origin = glm::vec3(); 
-		direction = glm::vec3();
-	}
-	Ray(glm::vec3 origin_in, glm::vec3 direction_in) : origin( origin_in ), direction( direction_in ) {};
+	Ray()
+		: origin(0.0), direction(0.0,0.0,1.0)
+	{}
+	Ray(glm::vec3 const& origin_in, glm::vec3 const& direction_in) : origin( origin_in ), direction( direction_in ) {};
 	glm::vec3 origin;
 	glm::vec3 direction;
 	glm::vec3 direction_inv;
