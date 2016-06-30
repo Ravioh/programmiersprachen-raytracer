b50afe6717a6cf678c2eb929d84bb3c7ac7b7c2e
ravioh <killuah@gmx.net>
2016-06-30 18:34:47 +0200

    rework

--- framework/shape.h
+++ framework/shape.h
@@ -3,25 +3,32 @@
 
 #include "material.h"
 #include "ray.h"
+#include <memory>
 #include <glm/glm.hpp>
+
+using namespace std; 
 
 class Shape {
 public:
 	Shape();
+	Shape(std::string const& n, Material const& m) : name_(n), material_(m) {}
 
-	virtual std::string getName() { return " "; };
-	virtual double intersect(Ray ray) { return 0.0; }
-	virtual Material getMaterial() const { return Material();  }
-	virtual glm::vec3 getNormalAt(glm::vec3 point) const { return glm::vec3(); }
-
+	std::string const& getName() const { return name_; };
+	virtual double intersect(Ray const& ray) const = 0;
+	virtual bool intersect_shadow(Ray const& ray) const = 0;
+	Material const& getMaterial() const { return material_;  }
+    virtual glm::vec3 getNormalAt(glm::vec3 const& point) const = 0;
 	void translate(glm::vec3 const& t);
 	void scale(glm::vec3 const& s);
 	void rotate(float deg, glm::vec3 const& axis);
 
+protected:
+  std::string name_;
+  Material material_;
 
 
-/*protected:
-  glm::vec3 world_normal(glm::vec3 const& n) const;
+
+/*  glm::vec3 world_normal(glm::vec3 const& n) const;
 
   Ray object_ray(Ray const& r) const;
 
